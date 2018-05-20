52fbe99a53d23824172170d5107cfa25fdf9dcbb
DanikNik <nikolsky.dan@gmail.com>
2018-05-21 00:19:46 +0300

    update

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     Deque<int> deq1 = {10, 2, 3, -4, -2, 5, -124, 102};
     Deque<int>::iterator it = deq1.Begin();
     Sort(deq1.Begin(), deq1.End());
-    cout << deq1;
+    cout << *it;
     cout << endl;
     return 0;
 }
